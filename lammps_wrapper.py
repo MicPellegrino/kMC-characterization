aa539199b6202df88dc7302ff20831e0f20195f0
Michele <michele.pellegrino@scilifelab.se>
2025-07-14 16:47:19 +0200

    Stub for topolgy wrapper

--- lammps_wrapper.py
+++ lammps_wrapper.py
@@ -12,3 +12,18 @@ def lammps_units(lmp) :
     atom_modify map array
     """
     lmp.commands_string(initialization_commands)
+
+def lammps_topology(lmp, substrate_file) :
+
+    lmp.command(f"read_data {substrate_file} extra/atom/types 1")
+
+    topology="""
+    group substrate type 1
+    group adatoms type 2
+    variable ffname string "test/CuAgAuNiPdPtAlPbFeMoTaWMgCoTiZr_Zhou04.eam.alloy"
+    pair_style eam/alloy 
+    pair_coeff * * ${ffname} Mo Mo
+    neighbor 2.0 bin
+    neigh_modify delay 0 every 1 check yes
+    """
+    lmp.commands_string(topology)
