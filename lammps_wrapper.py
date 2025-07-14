8a09447c8744689f14878ec30266cba7cb2a05a2
Michele <michele.pellegrino@scilifelab.se>
2025-07-14 16:55:43 +0200

    Wrapped output freq.

--- lammps_wrapper.py
+++ lammps_wrapper.py
@@ -13,7 +13,22 @@ def lammps_units(lmp) :
     """
     lmp.commands_string(initialization_commands)
 
-def lammps_topology(lmp, substrate_file) :
+def lammps_nstout(lmp, tout=125, ndump=25, nevery=25, nrepeat=10, nfreq=250, nrun=250) :
+
+    # Most of these should be dynamic
+    output_variables=f"""
+    variable tout equal {tout}
+    variable ndump equal {ndump}
+    variable nevery equal {nevery}
+    variable nrepeat equal {nrepeat}
+    variable nfreq equal {nfreq}
+    variable nrun equal {nrun}
+    """
+    lmp.commands_string(output_variables)
+
+def lammps_topology(lmp, substrate_file, na_sub=1, na_ada=1) :
+
+    """ Define inital coonfiguration, atom types and groups, force field """
 
     lmp.command(f"read_data {substrate_file} extra/atom/types 1")
 
